61496d5a54448e0017a7913a7b10975f0719216e
Max Nowack <max@unsou.de>
2017-03-02 08:29:14 +0100

    upgrade meteor version to 1.4.3.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'teamgrid:reactive-interval',
-  version: '1.0.1',
+  version: '1.0.1_1',
   // Brief, one-line summary of the package.
   summary: 'Reactively rerun functions based on a specified interval.',
   // URL to the Git repository containing the source code for this package.
@@ -11,7 +11,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3.2.1');
+  api.versionsFrom('1.4.3.1');
   api.use('ecmascript');
   api.use('tracker');
   api.mainModule('reactive-interval.js');
